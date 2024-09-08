38a6a7ef564094c37c65424cbd84fe4e6e282426
Baalateja Kataru <baalateja.k@gmail.com>
2024-09-08 15:25:32 +0000

    finished until 10

---
+++ youtube.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+
+int main() 
+{
+    int slices;
+
+    cout << "Y0 fatty how many pieces of pizza you eat?: ";
+
+    cin >> slices;
+
+    cout << "you have " << slices << " slices of pizza." << std::endl;
+}
