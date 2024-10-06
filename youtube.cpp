b3f2732e72bceaca4f347fe319d9ae9aaff92a71
Baalateja Kataru <baalateja.k@gmail.com>
2024-10-06 12:36:11 +0000

    checkpoint: 44 - creating a guessing game

--- youtube.cpp
+++ youtube.cpp
@@ -1,10 +1,53 @@
 #include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
 
-using std::cout;
-using std::endl;
+void play_game()
+{
+    int random = rand() % 251;
+    std::cout << random << std::endl;
+
+    while (true)
+    {
+        int guess;
+        std::cin >> guess;
+        
+        if (guess == random)
+        {
+            std::cout << "you win" << std::endl;
+            break;
+        } 
+        else if (guess < random)
+        {
+            std::cout << "too low" << std::endl;
+        }
+        else
+        {
+            std::cout << "too high" << std::endl;
+        }
+    }
+}
 
 int main()
 {
-    bool pizza_is_good = 0;
-    cout << pizza_is_good << endl;
+    srand(time(NULL));
+    int choice;
+
+    do {
+        std::cout << "0. Quit" << std::endl << "1. Play Game" << std::endl;
+        std::cin >> choice;
+
+        switch (choice)
+        {
+            case 0:
+                std::cout << "bye" << std::endl;
+                return 0;
+            case 1:
+                play_game();
+                break;
+        }
+    } while (choice != 0);
+
+
 }
